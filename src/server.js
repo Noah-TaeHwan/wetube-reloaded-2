0a8f72b925ad393b8f19addb8ac4461070a8f197
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-02-26 20:59:42 +0900

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -4,9 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log('Somebody is trying to go home.');
+const handleHome = (req, res) => {
+  console.log('Somebody is trying to go home.');
+  return res.send('Hello');
+};
+
+const handleLogin = (req, res) => {
+  return res.send('Login');
+};
 
 app.get('/', handleHome);
+app.get('/login', handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server Listening On Port http://localhost:${PORT} 🚀`);
