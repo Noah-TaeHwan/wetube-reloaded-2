71f12ba2085633ab001bc06db2169651f7435759
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-02-27 19:57:25 +0900

    #4.1 Making Our Routers

---
+++ README.md
@@ -0,0 +1,18 @@
+# Wetube Reloaded - 2
+
+global router
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+user router
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+video router
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -5,13 +5,23 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan('dev');
-
-const home = (req, res) => {
-  return res.end();
-};
-
 app.use(logger);
-app.get('/', home);
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send('Home');
+globalRouter.get('/', handleHome);
+
+const userRouter = express.Router();
+const handleUser = (req, res) => res.send('Edit User');
+userRouter.get('/edit', handleUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send('Watch Video');
+videoRouter.get('/watch', handleWatchVideo);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 const handleListening = () =>
   console.log(`✅ Server Listening On Port http://localhost:${PORT} 🚀`);
