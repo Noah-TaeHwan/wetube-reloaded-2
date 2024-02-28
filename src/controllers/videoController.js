8b7606878aee83a39d27881389c4a65717136bc2
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-02-28 21:30:02 +0900

    #5.10 Recap

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: 'Nico',
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: 'Video #1',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes ago',
+      views: 59,
+      id: 1,
+    },
+    {
+      title: 'Video #2',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes ago',
+      views: 59,
+      id: 2,
+    },
+    {
+      title: 'video #3',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes ago',
+      views: 59,
+      id: 3,
+    },
+  ];
+  return res.render('home', { pageTitle: 'Home', videos });
 };
-
-export const trending = (req, res) =>
-  res.render('home', { pageTitle: 'Home', fakeUser });
 export const see = (req, res) => res.render('watch', { pageTitle: 'Watch' });
 export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' });
 
