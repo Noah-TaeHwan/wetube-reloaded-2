565622ec6133cb72a7fe3df6157c5020e34b9ec2
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-03-01 21:26:45 +0900

    #6.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from '../models/video';
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: 'desc' });
   return res.render('home', { pageTitle: 'Home', videos });
 };
 
@@ -69,4 +69,17 @@ export const deleteVideo = async (req, res) => {
     return res.render('404', { pageTitle: `Not Found ${id}` });
   }
   return res.redirect('/');
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, 'i'),
+      },
+    });
+  }
+  return res.render('search', { pageTitle: `Search`, videos });
 };
