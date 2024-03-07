22e61f9fb4baa63c25f6660b25f728077a8f1b4a
Noah-TaeHwan <noah.taehwan@gmail.com>
2024-03-07 20:50:08 +0900

    #9.3 Webpack Configuration part Three

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,27 +6,30 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/client/js/main.js":
-/*!*******************************!*\
+/******/ (() => {
+  // webpackBootstrap
+  /******/ var __webpack_modules__ = {
+    /***/ './src/client/js/main.js':
+      /*!*******************************!*\
   !*** ./src/client/js/main.js ***!
   \*******************************/
-/***/ (() => {
+      /***/ () => {
+        eval(
+          'alert("hi");\n\n//# sourceURL=webpack://wetube/./src/client/js/main.js?'
+        );
 
-eval("const hello = async () => {\n  alert('hi! its working');\n  const x = await fetch('');\n};\nhello();\n\n//# sourceURL=webpack://wetube-reloaded-2/./src/client/js/main.js?");
+        /***/
+      },
 
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/client/js/main.js"]();
-/******/ 	
-/******/ })()
-;
+    /******/
+  };
+  /************************************************************************/
+  /******/
+  /******/ // startup
+  /******/ // Load entry module and return exports
+  /******/ // This entry module can't be inlined because the eval devtool is used.
+  /******/ var __webpack_exports__ = {};
+  /******/ __webpack_modules__['./src/client/js/main.js']();
+  /******/
+  /******/
+})();
